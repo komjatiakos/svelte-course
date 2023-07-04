9e9eeb1629dfc3a0677c50551fc9d2d1c2bdb433
Komjáti Ákos <komjati.akos@outlook.com>
2023-07-04 20:11:09 +0200

    reactive statements

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,15 @@
 <script>
     let count = 0
+    $: string =`You have clicked ${count} times`;
+
+    $: if(count >5){
+        console.log(`You have clicked ${count} times`)
+    }
     function increment () {
         count += 1
     }
 </script>
-
+<p>{string}</p>
 <button on:click={increment}>
     Clicks {count}
 </button>
