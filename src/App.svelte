800c47fc811bd02cc2456b1e7e5aba2044bc41cd
Komjáti Ákos <komjati.akos@outlook.com>
2023-07-04 19:42:09 +0200

    First real component :)

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,9 @@
 <script>
-    let name = "Akos";
-    let src = "http://picsum.com/200/200";
-    let string = 'Some string with html <strong>HELLO</strong>'
+    import Counter from "./lib/Counter.svelte";
 </script>
 
-<img src={src} alt="Some text ${name}"/>
-<h1>Hi, {name.toUpperCase()}!</h1>
-<p>{@html string}</p>
-<p>{string}</p>
+<Counter/>
 
 <style>
-    h1{
-        color:aqua
-    }
+
 </style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,17 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
-  }
+    let count = 0
+    function increment () {
+        count += 1
+    }
 </script>
 
 <button on:click={increment}>
-  count is {count}
+    Clicks {count}
 </button>
+
+<style>
+    button{
+        margin: 25%;
+        padding: 15px;
+    }
+</style>
